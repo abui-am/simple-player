ea3f8efa4f0a83d839e8427920dc59cf721e2a75
Abui <adjiem31@gmail.com>
2021-08-31 15:19:19 +0700

    faet: add queryParam title,url,logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import ReactPlayer from 'react-player/file';
+import { useLocation } from 'react-router-dom';
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function App() {
+  const query = useQuery();
+  const url = query.get("url");
+  const logo = query.get("logo");
+  const title = query.get("title");
+
   return (
     <div >
-
       <div className="App">
         <section style={{
           width: "100%",
@@ -30,10 +40,10 @@ function App() {
             <div style={{
               display: "flex"
             }}>
-              <a href="" className="nav-link">
+              <a href="https://play.svara.id" target="_blank" rel="noreferrer" className="nav-link">
                 Webplay
               </a>
-              <a href="" className="nav-link">
+              <a href="http://onelink.to/svarapp" target="_blank" rel="noreferrer" className="nav-link">
                 Get App
               </a>
             </div>
@@ -41,8 +51,10 @@ function App() {
         </section>
 
         <section style={{ width: "100%", background: "black", display: "block" }}>
-          <ReactPlayer url='http://45.118.114.26/camera/Tamansari.m3u8' controls
-
+          <ReactPlayer url={url}
+            controls
+            playing={true}
+            muted
             width={"100%"}
             height="80vh"
             config={{
@@ -56,15 +68,15 @@ function App() {
         <section className="wrapper" style={{ marginTop: 24, marginBottom: 24 }}>
           <div className="container">
             <div style={{ display: "flex" }}>
-              <img src="/images/logo-svara.svg"
+              <img src={logo}
                 alt="logo"
                 width={81}
                 height={81}
-                style={{ marginRight: 24 }}
+                style={{ marginRight: 24, border: "1px solid #e2e2e2", borderRadius: 8, background: "white" }}
               />
               <div>
                 <h2 className="title">
-                  Cipaganti - Video Streaming ATCS Kota Bandung
+                  {title}
                 </h2>
                 <button className="share-button"
                   onClick={() => { }}
